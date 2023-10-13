d0d9188d9c50ae509f2524d1e17f388897892c51
SirKnightFarron <mt@adepto.as>
2023-10-13 06:47:26 +0200

    Intro to React-native and add vscode to gitignore

--- FGfrontend/App.tsx
+++ FGfrontend/App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -13,6 +13,7 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   useColorScheme,
   View,
 } from 'react-native';
@@ -30,18 +31,20 @@ type SectionProps = PropsWithChildren<{
 }>;
 
 function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  let isDarkMode = useColorScheme() === 'dark';
+  
   return (
     <View style={styles.sectionContainer}>
       <Text
         style={[
           styles.sectionTitle,
           {
-            color: isDarkMode ? Colors.white : Colors.black,
+            color: isDarkMode ? Colors.white : Colors.black, // bool ? this if true :  that if false
           },
         ]}>
-        {title}
+        {title} section 
       </Text>
+
       <Text
         style={[
           styles.sectionDescription,
@@ -49,14 +52,16 @@ function Section({children, title}: SectionProps): JSX.Element {
             color: isDarkMode ? Colors.light : Colors.dark,
           },
         ]}>
-        {children}
+        SECTION DECRIPTION
       </Text>
+
     </View>
   );
 }
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const [name, setName] = useState("")
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -68,29 +73,30 @@ function App(): JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
+
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <Header />
+
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-            But why does this work with all those errors?!
+
+          <Section title={name}>
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
+          <TextInput  placeholder='input here' onChangeText={setName}></TextInput>
+
+          <Section title="zweite">
           </Section>
-          <Section title="Debug">
-            <DebugInstructions />
+
+          <Section title="dritte">
           </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
+
+          <Section title="vierte">
           </Section>
-          <LearnMoreLinks />
+
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -101,19 +107,26 @@ const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
+    borderRadius: 12,
+    backgroundColor: '#fac'
   },
+
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
   },
+
   sectionDescription: {
     marginTop: 8,
     fontSize: 18,
     fontWeight: '400',
+    backgroundColor: '#AFC'
   },
+
   highlight: {
     fontWeight: '700',
   },
+
 });
 
 export default App;
