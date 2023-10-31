dc0a42adac90399df98e4d7fb32252437ba494fb
SirKnightFarron <marvtrimpop@gmail.com>
2023-10-31 03:08:26 +0100

    Add SafeAreaProvider at root

--- FGfrontend/App.tsx
+++ FGfrontend/App.tsx
@@ -6,19 +6,27 @@
  */
 
 import React from 'react';
+import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import OverviewScreen from './src/screens/OverviewScreen';
 
 const Stack = createNativeStackNavigator();
 
+const styles = {
+  container: {
+    flex: 1
+  }
+}
+
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Overview" component={OverviewScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Overview" component={OverviewScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
