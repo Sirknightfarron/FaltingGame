3731b6954ea345f374cfe17c9b5a1080b367a90e
SirKnightFarron <marvtrimpop@gmail.com>
2023-10-31 03:04:34 +0100

    Fix SafeAreaProvider not rendering app, add Gestures

--- FGfrontend/index.js
+++ FGfrontend/index.js
@@ -1,9 +1,14 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-AppRegistry.registerComponent(appName, () => App);
+const AppWithSafeArea = () => {
+    return (
+        <SafeAreaProvider>
+            <App />
+        </SafeAreaProvider>
+    );
+}
+
+AppRegistry.registerComponent(appName, () => AppWithSafeArea);
