719fdc4f4dfa37f2b092e86d47b54250c6a1e758
SirKnightFarron <marvtrimpop@gmail.com>
2024-01-11 19:02:54 +0100

    Add Screen to app

--- FGfrontend/App.js
+++ FGfrontend/App.js
@@ -1,29 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'; 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OverviewScreen from './src/screens/OverviewScreen';
 import JoinScreen from './src/screens/JoinScreen';
 import CreateScreen from './src/screens/CreateScreen';
+import GameScreen from './src/screens/GameScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Overview">
-        <Stack.Screen name="Overview" component={OverviewScreen} />
+      <Stack.Navigator initialRouteName='Overview'>
+        <Stack.Screen name='Overview' component={OverviewScreen} />
         <Stack.Screen name='Join' component={JoinScreen} />
         <Stack.Screen name='Create' component={CreateScreen} />
+        <Stack.Screen name='Game' component={GameScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
