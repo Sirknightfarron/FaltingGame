8b7fb4e3a7f95f316fff03be90761ae51a57993c
SirKnightFarron <marvtrimpop@gmail.com>
2024-01-19 19:28:45 +0100

    Add the last message container to GameScreen

--- FGfrontend/src/screens/GameScreen.tsx
+++ FGfrontend/src/screens/GameScreen.tsx
@@ -1,25 +1,41 @@
-import { Text, StyleSheet, TextInput, View } from "react-native";
+import { Text, StyleSheet, TextInput, View, Dimensions } from "react-native";
 import Content from "../components/Content";
 import BaseScreen from "../components/BaseScreen";
 import Button from "../components/Button";
+import React, { useState } from "react";
 
-const GameScreen = ({ route }) => {
+const GameScreen = ({ route, navigation }) => {
+    const { gameId, gameTitle, gamePlayersAmount, messages } = route.params;
+    const [ userInput, setUserInput ] = useState('');
+    const [ lastMessage, setLastMessage ] = useState('This is the first message');
+
     const handleSubmit = () => {
-
+        setLastMessage(userInput)
     }
 
     return (
-        <BaseScreen route={ route } >
+        <BaseScreen route={route} >
             <Content style={styles.content}>
-                <Text style={styles.heading}>Game Screen</Text>
-                <TextInput 
-                    maxLength={120}
+                <Text style={styles.heading}>{gameTitle}</Text>
+                <View style={styles.lastMessageContainer}>
+                    <Text style={styles.lastMessageTitle}>Last Message: </ Text>
+                    <Text> {lastMessage}</Text>
+                </View>
+                <TextInput
+                    maxLength={300}
                     multiline
-                    style={styles.textInput}
+                    style={ styles.textInput }
+                    placeholder="Enter your wonderful idea here!"
+                    onChangeText={setUserInput}
+                    value={ userInput }
                 />
+                <View style={styles.infoContainer}>
+                    <Text>#Players: { gamePlayersAmount }</Text>
+                    <Text>ID: { gameId }</Text>
+                </View>
                 <View style={styles.buttonContainer}>
-                    <Button 
-                        buttonText='Submit' 
+                    <Button
+                        buttonText='Submit'
                         onPress={handleSubmit}
                         style={styles.buttons}
                     />
@@ -43,10 +59,12 @@ const styles = StyleSheet.create({
     },
     textInput: {
         width: '100%',
-        height: '70%',
+        height: Dimensions.get('window').height/2,
         backgroundColor: '#def',
         borderWidth: 2,
         borderRadius: 12,
+        padding: 12,
+        fontSize: 18
     },
     buttonContainer: {
         flex: 1,
@@ -61,6 +79,16 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         padding: 20,
         top: 10
+    },
+    infoContainer:{
+        flexDirection: 'row',
+        gap: Dimensions.get('window').height*0.2,
+    },
+    lastMessageContainer: {
+        height: Dimensions.get('window').height*0.07
+    },
+    lastMessageTitle: {
+        fontWeight: 'bold'
     }
 })
 
