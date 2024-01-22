583e1ba0f17599a7941ea374b378bf0e04784fda
SirKnightFarron <marvtrimpop@gmail.com>
2024-01-22 20:04:27 +0100

    Add maxRounds and messages to game screen update ScreenProps styling

--- FGfrontend/src/screens/GameScreen.tsx
+++ FGfrontend/src/screens/GameScreen.tsx
@@ -2,36 +2,44 @@ import { Text, StyleSheet, TextInput, View, Dimensions } from "react-native";
 import Content from "../components/Content";
 import BaseScreen from "../components/BaseScreen";
 import Button from "../components/Button";
-import React, { useState } from "react";
+import { GameScreenProps } from "../types";
+import { useState } from "react";
 
-const GameScreen = ({ route, navigation }) => {
-    const { gameId, gameTitle, gamePlayersAmount, messages } = route.params;
+const GameScreen = ({ route, navigation }: GameScreenProps) => {
+    const { gameId, gameTitle, gamePlayersAmount, gameMaxRounds, messages } = route.params;
     const [ userInput, setUserInput ] = useState('');
     const [ lastMessage, setLastMessage ] = useState('This is the first message');
-
+    const maxMessageLength = 300;
+    
     const handleSubmit = () => {
         setLastMessage(userInput)
     }
 
+    // #TODO make Last message scrollable (or lower maxMessageLength everthing should be readable //maybe autoscroll last message? )
     return (
         <BaseScreen route={route} >
             <Content style={styles.content}>
                 <Text style={styles.heading}>{gameTitle}</Text>
                 <View style={styles.lastMessageContainer}>
                     <Text style={styles.lastMessageTitle}>Last Message: </ Text>
-                    <Text> {lastMessage}</Text>
+                    <Text style={styles.lastMessage}> { lastMessage }</Text>
                 </View>
-                <TextInput
-                    maxLength={300}
-                    multiline
-                    style={ styles.textInput }
-                    placeholder="Enter your wonderful idea here!"
-                    onChangeText={setUserInput}
-                    value={ userInput }
-                />
+                <View style={ styles.textInputContainer }>
+                    <TextInput
+                        maxLength={maxMessageLength}
+                        multiline
+                        style={ styles.textInput }
+                        placeholder="Enter your wonderful idea here!"
+                        onChangeText={ setUserInput }
+                        value={ userInput }
+                        keyboardType={"default"}
+                    />
+                    <Text style={styles.messageLength}> {userInput.length + '/' + maxMessageLength} </Text>
+                </View>
                 <View style={styles.infoContainer}>
-                    <Text>#Players: { gamePlayersAmount }</Text>
                     <Text>ID: { gameId }</Text>
+                    <Text>#Players: { gamePlayersAmount }</Text>
+                    <Text>#Rounds: { gameMaxRounds }</Text>
                 </View>
                 <View style={styles.buttonContainer}>
                     <Button
@@ -58,13 +66,23 @@ const styles = StyleSheet.create({
         padding: 20
     },
     textInput: {
+        paddingLeft: 20,
+        fontSize: 18
+    },
+    textInputContainer: {
         width: '100%',
-        height: Dimensions.get('window').height/2,
         backgroundColor: '#def',
         borderWidth: 2,
-        borderRadius: 12,
+        borderBottomLeftRadius: 12,
+        borderBottomRightRadius: 12,
+        height: Dimensions.get('window').height*0.484,
         padding: 12,
-        fontSize: 18
+    },
+    messageLength:{
+        fontSize: 12,
+        alignItems: 'flex-end',
+        height: '20%',
+        color: '#888'
     },
     buttonContainer: {
         flex: 1,
@@ -82,13 +100,27 @@ const styles = StyleSheet.create({
     },
     infoContainer:{
         flexDirection: 'row',
-        gap: Dimensions.get('window').height*0.2,
+        gap: Dimensions.get('window').height*0.07,
+        padding: 4
     },
     lastMessageContainer: {
-        height: Dimensions.get('window').height*0.07
+        backgroundColor: '#def',
+        borderWidth: 2,
+        borderBottomWidth: 0,
+        borderTopLeftRadius: 12,
+        borderTopRightRadius: 12,
+        padding: 6,
+        height: Dimensions.get('window').height*0.116,
+        width: '100%',
     },
     lastMessageTitle: {
         fontWeight: 'bold'
+    },
+    lastMessage: {
+        padding: 4,
+        flexDirection: 'column',
+        alignItems: 'flex-end',
+        justifyContent: 'flex-end',
     }
 })
 
